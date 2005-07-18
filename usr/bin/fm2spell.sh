63981781d516ce3c566c4899ab4fbbab35c4c4fb
Sergey A. Lipnevich <sergey@sourcemage.org>
2005-07-18 14:05:15 -0100

    Minor problems and error stream output corrected; can be used for spell creation now

--- usr/bin/fm2spell.sh
+++ usr/bin/fm2spell.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 declare -r SCRIPT_DIR=$(cd "$(dirname $0)" && pwd)
-[[ -x "$SCRIPT_DIR/$(basename $0)" ]] || echo Script directory not found &>2
+[[ -x "$SCRIPT_DIR/$(basename $0)" ]] || echo Script directory not found >&2
 declare -r SCRIPT_ROOT=$(cd "$SCRIPT_DIR/../.." && pwd)
 declare -r SCRIPT_VAR="$SCRIPT_ROOT/var/lib/quill"
-[[ -d "$SCRIPT_VAR" ]] || echo Script modules directory not found &>2
-declare -r SCRIPT_TMP="${TMP:-$SCRIPT_ROOT/tmp}"
-[[ -d "$SCRIPT_TMP" ]] || echo Script temporary directory not found &>2
+[[ -d "$SCRIPT_VAR" ]] || echo Script modules directory not found >&2
+declare -r SCRIPT_TMP="${TMP:-$SCRIPT_DIR}"
+[[ -d "$SCRIPT_TMP" ]] || echo Script temporary directory not found >&2
 
 declare -r MKSPELL="$SCRIPT_TMP/MKSPELL.$$.sh"
 
@@ -20,5 +20,6 @@ xsltproc $PARAM_PERSIST $PARAM_DEBUG $PARAM_PROJECT \
   "$SCRIPT_VAR/fm2spell.xsl" \
   "$SCRIPT_VAR/dummy.xml" \
   > "$MKSPELL" &&
+echo Running $MKSPELL... >&2 &&
 source "$MKSPELL" &&
 rm -f "$MKSPELL"
