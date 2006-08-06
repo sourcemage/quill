7bc939f367ea1c654d4ca8af4a2f72a5f6b743cc
Andraž Levstik <ruskie@mages.ath.cx>
2006-08-06 23:53:45 +0200

    minor fixup

--- generate_tarball.sh
+++ generate_tarball.sh
@@ -6,4 +6,4 @@ cd ..
 cp -r $CURRENTPWD/* quill-$MY_VERSION/
 rm -rf quill-$MY_VERSION/.git quill-$MY_VERSION/usr/bin/quill-old
 tar -jcvf quill-$MY_VERSION.tar.bz2 quill-$MY_VERSION
-gpg --digest-algo SHA512 -u 04E44296 -b quill-$MY_VERSION.tar.bz2
+gpg --digest-algo SHA512 -u 117869C9 -b quill-$MY_VERSION.tar.bz2
